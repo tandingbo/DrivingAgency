f727ab0b0f4fafeb8ac5a1211d322c34473ef147
fuyunwang <beautifulsoup@163.com>
2019-04-02 09:25:25 +0800

    Project Init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DrivingAgency
+
 驾校代理小程序后端
+
+## 技术包括
+
+Spring Boot 2.1.3
+
+Spring Data Jpa
+
+Spring Data Redis
---
+++ src/main/java/com/beautifulsoup/driving/DrivingApplication.java
@@ -0,0 +1,13 @@
+package com.beautifulsoup.driving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivingApplication.class, args);
+    }
+
+}
